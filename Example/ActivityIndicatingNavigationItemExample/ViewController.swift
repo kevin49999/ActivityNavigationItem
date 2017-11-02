140610ef413a87450f2696b3f4d8bbc4735b3e20
Kevin Johnson <kevinjohnsonutx@gmail.com>
2017-11-02 15:15:02 -0400

    Remove cocoapods from example

--- Example/ActivityIndicatingNavigationItemExample/ViewController.swift
+++ Example/ActivityIndicatingNavigationItemExample/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import ActivityIndicatingNavigationItem
 
 class ViewController: UIViewController {
 
--- Example/Pods/Target Support Files/Pods-ActivityIndicatingNavigationItemExample/Pods-ActivityIndicatingNavigationItemExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ActivityIndicatingNavigationItemExample : NSObject
-@end
-@implementation PodsDummy_Pods_ActivityIndicatingNavigationItemExample
-@end
