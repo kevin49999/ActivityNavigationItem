7d68a38c467fdca3badbeb2e47f94c2d97b34d8a
Kevin Johnson <kevinjohnsonutx@gmail.com>
2020-10-11 22:06:32 -0500

    support SPM

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ActivityNavigationItem",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ActivityNavigationItem",
+            targets: ["ActivityNavigationItem"]),
+    ],
+    targets: [
+        .target(
+            name: "ActivityNavigationItem",
+            path: "./Source"
+        ),
+        .testTarget(
+            name: "ActivityNavigationItemTests",
+            dependencies: ["ActivityNavigationItem"],
+            path: "./Tests"
+        ),
+    ]
+)
